a785895f525d336a89fdc0f8b49aaf8f7078da3d
Emmaceejay <ojikeya.chijioke@gmail.com>
2023-05-15 00:07:44 +0100

    modify CSS & javascript file - more functionality

--- scripts/script.js
+++ scripts/script.js
@@ -13,9 +13,14 @@ const playerChoice = document.querySelector('.player-choice');
 const computerChoice = document.querySelector('.computer-choice');
 const computerScore = document.querySelector('.computer-score');
 const playerScore = document.querySelector('.player-score');
-const champion = document.querySelector('.champion');
+const roundWinner = document.querySelector('.round-winner');
 const startPage = document.querySelector('.container');
 const startButton = document.querySelector('#start-button');
+const gameContainer = document.querySelector('#game-container');
+const endContainer = document.querySelector('.end-container');
+const result = document.querySelector('.result');
+const playerImage = document.querySelector('#player-image')
+const compImage = document.querySelector('comp-image')
 
 //computer input
 function computerPick(){
@@ -24,11 +29,43 @@ function computerPick(){
     return randomPick[randomRange];
 }
 
+//function to toggle computerimage to visible
+function compWinImage(){
+    compImage.classList.toggle('visible');
+}
+
+//function to toggle playerimage to visible
+function playerWinImage(){
+    playerImage.classList.toggle('visible');
+}
+
+//fumction to hide gamecontainer
+function hideGameContainer (){
+    gameContainer.style.opacity = 0;
+    gameContainer.style.transform = 'scale(0)';
+    gameContainer.style.display = "none";
+}
+
+
+
+//funtion to hide startpage and end container and reveal main game container
 startButton.addEventListener('click', function(){
     startPage.style.opacity = 0;
     startPage.style.transform = 'scale(0)';
     startPage.style.display = 'none';
+//end container hide
+    endContainer.style.opacity = 0;
+    startPage.style.transform = 'scale(0)';
+    startButton,style.display = 'none';
+
 })
+
+//function to reveal end container
+function revealEndContainer(){
+    endContainer.style.opacity = 100;
+    // endContainer.style.transform ='scaley(5)'
+    endContainer.style.display = 'flex';
+}
 
 //player input
 buttons.forEach((button) =>{
@@ -70,8 +107,8 @@ function playRound(playerSelection, computerSelection){
     
     winner(playerWins, computerWins);
    
-    computerScore.textContent= `Computer score: ${computerWins}`;
-    playerScore.textContent = `Player score: ${playerWins}`;
+    computerScore.textContent= ` ${computerWins}`;
+    playerScore.textContent = ` ${playerWins}`;
     playerChoice.textContent = `Player choice: ${(playerSelection).toUpperCase()}`;
     computerChoice.textContent = `Computer choice: ${(computerSelection).toUpperCase()}`;
     // console.log('computer wins: ' + computerWins);
@@ -86,11 +123,25 @@ function winner(a, b){
     if (a ==3 || b == 3){
         let anounce = "";
         if(a > b){
+            hideGameContainer ();
+            revealEndContainer();
             console.log(anounce = "you win round");
+            roundWinner.textContent = `You won this round`;
             console.log(playerWins + ":" + computerWins)
+            result.textContent = ` ${playerWins} : ${computerWins} `;
+            // playerImage.classList.toggle('visible');
+            playerWinImage()
+            
         }else if(b > a){
+            hideGameContainer ();
+            revealEndContainer();
             console.log(anounce = "computer win round");
+            roundWinner.textContent = `Computer won this round`;
             console.log(computerWins + ":" + playerWins)
+            result.textContent = ` ${computerWins} : ${playerWins} `;
+            // compImage.classList.toggle('visible');
+            compWinImage()
+            
         }   
         return anounce;
     }
